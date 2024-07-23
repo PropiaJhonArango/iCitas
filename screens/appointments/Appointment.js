caeed4c72482bb0d31b5b91ca218726eb96c7d6c
JhonArangoToro <78990684+JhonArangoToro@users.noreply.github.com>
2024-07-23 15:42:38 -0500

    Conclusion Rama principal
    Conclusion Rama principal

--- screens/appointments/Appointment.js
+++ screens/appointments/Appointment.js
@@ -450,7 +450,7 @@ export default function Appointment({ navigation, route }) {
             />
           }
         />
-
+        {/* Imagenes */}
         <View style={styles.viewBody}>
           <Text style={styles.textImage}>Imagenes</Text>
           <ScrollView horizontal style={styles.viewImage}>
